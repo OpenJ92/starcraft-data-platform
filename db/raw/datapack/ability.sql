ca513581075d6ae86e8d83729d571b9f162424fe
OpenJ92 <jacob.vartuli.92@gmail.com>
2024-12-13 11:40:10 -0500

    update schema and model to conform to psql lower casing

--- db/raw/datapack/ability.sql
+++ db/raw/datapack/ability.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS datapack.ABILITY (
+CREATE TABLE IF NOT EXISTS datapack.ability (
 	__id__ SERIAL PRIMARY KEY,        -- Auto-incrementing primary key
     	release_string TEXT,              -- Version of Starcraft
     	id INTEGER,                       -- Abilities ID in the game
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS datapack.ABILITY (
     	title TEXT,                       -- printable title of this abililty
     	is_build BOOLEAN,                 -- Boolean flagging this ability as creating a new unit
     	build_time INTEGER,               -- The number of seconds required to build this unit. 0 if not ``is_build``
-    	__UNIT_TYPE__ INTEGER,            -- A reference to the table `Unit_Type` that built by this ability. Default to None.
-    	CONSTRAINT ability_unit_type_fkey FOREIGN KEY (__UNIT_TYPE__)
+    	__unit_type__ INTEGER,            -- A reference to the table `Unit_Type` that built by this ability. Default to None.
+    	CONSTRAINT ability_unit_type_fkey FOREIGN KEY (__unit_type__)
     	    REFERENCES datapack.UNIT_TYPE (__id__) ON DELETE SET NULL
 );
